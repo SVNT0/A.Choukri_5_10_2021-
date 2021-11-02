7bce73fbee0ee0157e0749bb2c6dce00f6fa5ec4
Anas <anas_choukri@hotmail.fr>
2021-11-02 16:09:27 +0100

    Entrée données dans le DOM

--- front/js/script.js
+++ front/js/script.js
@@ -3,3 +3,17 @@ async function getArticles() {
     var articlesCatch = await fetch("http://localhost:3000/api/products")
     return await articlesCatch.json();
 }
+
+
+    // Répartition des données de l'API dans le DOM
+    async function fillSection() {
+        var result = await getArticles ()
+        .then(function (resultatAPI){
+            const articles = resultatAPI;
+            console.table(articles);
+            for (let article in articles) {
+    
+                // Insertion de l'élément "a"
+                let productLink = document.createElement("a");
+                document.querySelector(".items").appendChild(productLink);
+                productLink.href = `product.html?id=${resultatAPI[article]._id}`;

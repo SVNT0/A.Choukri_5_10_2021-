9e6094f40cd1c708e3ccc5c701dc876c9cc4faed
Anas <anas_choukri@hotmail.fr>
2021-11-08 14:40:20 +0100

    commencement product.js

---
+++ front/js/product.js
@@ -0,0 +1,30 @@
+var str = window.location.href;
+var url = new URL(str);
+var idProduct = url.searchParams.get("id");
+console.log(idProduct);
+let article = "";
+
+const colorPicked = document. querySelector("#colors");
+const quantityPicked = document.querySelector("#quantity");
+
+getArticle();
+
+// Récupération des articles issus de l'API
+function getArticle() {
+    fetch("http://localhost:3000/api/products/" + idProduct)
+    .then((res) => {
+        return res.json();
+    })
+
+    // Répartition des données issus de l'API dans le DOM
+    .then(async function (resultatAPI) {
+        article = await resultatAPI;
+        console.table(article);
+        if (article){
+            getPost(article);
+        }
+    })
+    .catch((error) => {
+        console.log("Erreur de la requête API");
+    })
+}

85bfc8ecbb3bd1d0016fbf72e7963e12e2d1e334
Anas <anas_choukri@hotmail.fr>
2021-11-12 14:34:48 +0100

    insertion éléments panier 3

--- front/js/cart.js
+++ front/js/cart.js
@@ -36,3 +36,44 @@ for (let produit in produitLocalStorage){
     let productItemContentTitlePrice = document.createElement("div");
     productItemContent.appendChild(productItemContentTitlePrice);
     productItemContentTitlePrice.className = "cart__item__content__titlePrice";
+
+        // Insertion du titre h3
+        let productTitle = document.createElement("h2");
+        productItemContentTitlePrice.appendChild(productTitle);
+        productTitle.innerHTML = produitLocalStorage[produit].nomProduit;
+    
+        // Insertion de la couleur
+        let productColor = document.createElement("p");
+        productTitle.appendChild(productColor);
+        productColor.innerHTML = produitLocalStorage[produit].couleurProduit;
+        productColor.style.fontSize = "20px";
+    
+        // Insertion du prix
+        let productPrice = document.createElement("p");
+        productItemContentTitlePrice.appendChild(productPrice);
+        productPrice.innerHTML = produitLocalStorage[produit].prixProduit + " €";
+    
+        // Insertion de l'élément "div"
+        let productItemContentSettings = document.createElement("div");
+        productItemContent.appendChild(productItemContentSettings);
+        productItemContentSettings.className = "cart__item__content__settings";
+    
+        // Insertion de l'élément "div"
+        let productItemContentSettingsQuantity = document.createElement("div");
+        productItemContentSettings.appendChild(productItemContentSettingsQuantity);
+        productItemContentSettingsQuantity.className = "cart__item__content__settings__quantity";
+        
+        // Insertion de "Qté : "
+        let productQte = document.createElement("p");
+        productItemContentSettingsQuantity.appendChild(productQte);
+        productQte.innerHTML = "Qté : ";
+    
+        // Insertion de la quantité
+        let productQuantity = document.createElement("input");
+        productItemContentSettingsQuantity.appendChild(productQuantity);
+        productQuantity.value = produitLocalStorage[produit].quantiteProduit;
+        productQuantity.className = "itemQuantity";
+        productQuantity.setAttribute("type", "number");
+        productQuantity.setAttribute("min", "1");
+        productQuantity.setAttribute("max", "100");
+        productQuantity.setAttribute("name", "itemQuantity");

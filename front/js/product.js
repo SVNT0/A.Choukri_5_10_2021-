1e15c0437251c2090c1702f0785113a3f0b3ddb3
Anas <anas_choukri@hotmail.fr>
2021-12-15 16:03:56 +0100

    correction erreur panier

--- front/js/product.js
+++ front/js/product.js
@@ -4,19 +4,19 @@ var idProduct = url.searchParams.get("id");
 console.log(idProduct);
 let article = "";
 
-const colorPicked = document. querySelector("#couleurs");
-const quantityPicked = document.querySelector("#quantité");
+const colorPicked = document. querySelector("#colors");
+const quantityPicked = document.querySelector("#quantity");
 
 getArticle();
 
-// Récupération des articles issus de l'API
+// Récupération des articles de l'API
 function getArticle() {
     fetch("http://localhost:3000/api/products/" + idProduct)
     .then((res) => {
         return res.json();
     })
 
-    // Répartition des données issus de l'API dans le DOM
+    // Répartition des données de l'API dans le DOM
     .then(async function (resultatAPI) {
         article = await resultatAPI;
         console.table(article);
@@ -28,27 +28,27 @@ function getArticle() {
         console.log("Erreur de la requête API");
     })
 }
-
+    
 function getPost(article){
-    // Insertion de l'élément image
-    let productImg = document.createElement("image");
+    // Insertion de l'image
+    let productImg = document.createElement("img");
     document.querySelector(".item__img").appendChild(productImg);
     productImg.src = article.imageUrl;
     productImg.alt = article.altTxt;
 
-    // Titre "h1"
+    // Modification du titre "h1"
     let productName = document.getElementById('title');
     productName.innerHTML = article.name;
 
-    // Prix
+    // Modification du prix
     let productPrice = document.getElementById('price');
     productPrice.innerHTML = article.price;
 
-    // Description
+    // Modification de la description
     let productDescription = document.getElementById('description');
     productDescription.innerHTML = article.description;
 
-    // Insertion des différentes options de couleurs
+    // Insertion des options de couleurs
     for (let colors of article.colors){
         console.table(colors);
         let productColors = document.createElement("option");
@@ -59,7 +59,7 @@ function getPost(article){
     addToCart(article);
 }
 
-//Gestion du panier de commande
+//Gestion du panier
 function addToCart(article) {
     const btn_envoyerPanier = document.querySelector("#addToCart");
 
@@ -67,13 +67,13 @@ function addToCart(article) {
     btn_envoyerPanier.addEventListener("click", (event)=>{
         if (quantityPicked.value > 0 && quantityPicked.value <=100 && quantityPicked.value != 0){
 
-    //Choix de la couleur
+    //Recupération du choix de la couleur
     let choixCouleur = colorPicked.value;
                 
-    //Choix de la quantité
+    //Recupération du choix de la quantité
     let choixQuantite = quantityPicked.value;
 
-    //Récupération des infos de l'article pour l'ajouter au panier
+    //Récupération des options de l'article à ajouter au panier
     let optionsProduit = {
         idProduit: idProduct,
         couleurProduit: choixCouleur,
@@ -88,9 +88,9 @@ function addToCart(article) {
     //Initialisation du local storage
     let produitLocalStorage = JSON.parse(localStorage.getItem("produit"));
 
-    //fenêtre pop-up de confirmation
+    //fenêtre pop-up
     const popupConfirmation =() =>{
-        if(window.confirm(`Votre commande de ${choixQuantite} ${article.name} ${choixCouleur} a été ajoutée au panier
+        if(window.confirm(`Votre commande de ${choixQuantite} ${article.name} ${choixCouleur} est ajoutée au panier
 Pour consulter votre panier, cliquez sur OK`)){
             window.location.href ="cart.html";
         }
